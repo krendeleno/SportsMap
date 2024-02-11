26b3b28a3f954567bab834c38ac4b72ff5f810e7
krendeleno <krendeleno@gmail.com>
2024-02-11 11:13:27 +0300

    fix: fix routes

--- nginx/deploy.sh
+++ nginx/deploy.sh
@@ -7,17 +7,14 @@ TOKEN=$(curl -d "{\"yandexPassportOauthToken\":\"${OAUTH}\"}" "https://iam.api.c
 IAM_TOKEN=$(echo "${TOKEN}"  | awk '{print $2}' | sed 's/^"\(.*\)",*/\1/')
 
 mkdir certs
-cd ./certs
 
 # жуткие костыли для работы с json и jq
-curl -H "Authorization: Bearer ${IAM_TOKEN}" https://data.certificate-manager.api.cloud.yandex.net/certificate-manager/v1/certificates/${CERTIFICATE_ID}:getContent | jq -r '.privateKey'  | sed 's/\\[tn]//g; s/"\(.*\)"/\1/' > key.txt
-sed -i '' -e '$ d' ./key.txt
-curl -H "Authorization: Bearer ${IAM_TOKEN}" https://data.certificate-manager.api.cloud.yandex.net/certificate-manager/v1/certificates/${CERTIFICATE_ID}:getContent | jq '.certificateChain' | jq -r '.[0]'  | sed 's/\\[tn]//g; s/"\(.*\)"/\1/' > bundle.crt
-sed -i '' -e '$ d' ./bundle.crt
-curl -H "Authorization: Bearer ${IAM_TOKEN}" https://data.certificate-manager.api.cloud.yandex.net/certificate-manager/v1/certificates/${CERTIFICATE_ID}:getContent | jq '.certificateChain' | jq -r '.[1]' | sed 's/\\[tn]//g; s/"\(.*\)"/\1/' >> bundle.crt
-sed -i '' -e '$ d' ./bundle.crt
-
-cd ../
+curl -H "Authorization: Bearer ${IAM_TOKEN}" https://data.certificate-manager.api.cloud.yandex.net/certificate-manager/v1/certificates/${CERTIFICATE_ID}:getContent | jq -r '.privateKey'  | sed 's/\\[tn]//g; s/"\(.*\)"/\1/' > certs/key.txt
+sed -i '' -e '$ d' certs/key.txt
+curl -H "Authorization: Bearer ${IAM_TOKEN}" https://data.certificate-manager.api.cloud.yandex.net/certificate-manager/v1/certificates/${CERTIFICATE_ID}:getContent | jq '.certificateChain' | jq -r '.[0]'  | sed 's/\\[tn]//g; s/"\(.*\)"/\1/' > certs/bundle.crt
+sed -i '' -e '$ d' certs/bundle.crt
+curl -H "Authorization: Bearer ${IAM_TOKEN}" https://data.certificate-manager.api.cloud.yandex.net/certificate-manager/v1/certificates/${CERTIFICATE_ID}:getContent | jq '.certificateChain' | jq -r '.[1]' | sed 's/\\[tn]//g; s/"\(.*\)"/\1/' >> certs/bundle.crt
+sed -i '' -e '$ d' certs/bundle.crt
 
 echo "${OAUTH}" | docker login --username oauth --password-stdin cr.yandex
 
